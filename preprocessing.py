617bab4ff98e0158d765e806b0ad28f633475aa1
Pornphawit Manasut <wit@Pornphawits-MacBook-Air.local>
2023-12-11 10:21:05 +0100

    fixed so image preprocessing takes in color images too

--- preprocessing.py
+++ preprocessing.py
@@ -1,22 +1,25 @@
 from abc import ABC, abstractmethod
+from typing import List
+
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
 
-
 class ImagePreprocessor(ABC):
     @abstractmethod
-    def preprocess(self, image):
+    def preprocess(self, image_grayscale=None, image_color=None):
         pass
 
-    def show_images(self, image):
-        prepro_img = image.copy()
-        prepro_img = self.preprocess(prepro_img)
-        fig, axes = plt.subplots(1, 2, figsize=(20,20))
+    def show_images(self, image_grayscale=None, image_color=None):
+        if image_grayscale is None and image_color is None:
+            raise Exception("NO input images!")
+        prepro_img = image_grayscale.copy()
+        prepro_img = self.preprocess(prepro_img, image_color.copy())
+        fig, axes = plt.subplots(1, 2, figsize=(20, 20))
         axes = axes.flatten()
-        axes[0].imshow(image, cmap='gray', vmin = 0, vmax = 255)
-        axes[1].imshow(prepro_img, cmap='gray', vmin = 0, vmax = 255)
+        axes[0].imshow(image_grayscale, cmap='gray', vmin=0, vmax=255)
+        axes[1].imshow(prepro_img, cmap='gray', vmin=0, vmax=255)
         return prepro_img
 
 
@@ -27,18 +30,22 @@ class BackgroundPreprocessor(ImagePreprocessor):
         cv2.fillPoly(self.background_mask, [self.background_points], color=255)
         self.background_mask = cv2.bitwise_not(self.background_mask)
 
-    def preprocess(self, image):
+    def preprocess(self, image_grayscale=None, image_color=None):
         # Create a mask for the background points
-        return cv2.bitwise_and(image, image, mask=self.background_mask)
+        if image_grayscale is None:
+            raise Exception("No input grayscale image!")
+        return cv2.bitwise_and(image_grayscale, image_grayscale, mask=self.background_mask)
 
 
 class ImageAveragingPreprocessor(ImagePreprocessor):
     def __init__(self, background_images):
-        self.background_images = [cv2.GaussianBlur(i, ksize=(7,7), sigmaX=0) for i in background_images]
+        self.background_images = [cv2.GaussianBlur(i, ksize=(7, 7), sigmaX=0) for i in background_images]
         self.average_image = np.mean(self.background_images, axis=0).astype(np.uint8)
 
-    def preprocess(self, image):
-        return image - self.average_image # image - self.average_image #  cv2.absdiff(image, self.average_image)
+    def preprocess(self, image_grayscale=None, image_color=None):
+        if image_grayscale is None:
+            raise Exception("No input grayscale image!")
+        return image_grayscale - self.average_image
 
 
 class ThresholdingPreprocessor(ImagePreprocessor):
@@ -46,10 +53,12 @@ class ThresholdingPreprocessor(ImagePreprocessor):
         self.lower_threshold = lower_threshold
         self.upper_threshold = upper_threshold
 
-    def preprocess(self, image):
+    def preprocess(self, image_grayscale=None, image_color=None):
+        if image_grayscale is None:
+            raise Exception("No input grayscale image!")
         # Create an inRange mask based on threshold values
-        inrange_mask = cv2.inRange(image, self.lower_threshold, self.upper_threshold)
-        img = cv2.bitwise_and(image, image, mask=inrange_mask)
+        inrange_mask = cv2.inRange(image_grayscale, self.lower_threshold, self.upper_threshold)
+        img = cv2.bitwise_and(image_grayscale, image_grayscale, mask=inrange_mask)
 
         # Apply Otsu's thresholding to further clean the image
         _, img = cv2.threshold(img, self.lower_threshold, self.upper_threshold, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
@@ -57,25 +66,29 @@ class ThresholdingPreprocessor(ImagePreprocessor):
 
 
 class WaterThresholdingPreprocessor(ImagePreprocessor):
-    def __init__(self, lower_blue = [90, 40, 40], upper_blue = [130, 255, 255]) -> None:
+    def __init__(self, lower_blue=[90, 40, 40], upper_blue=[130, 255, 255]) -> None:
         self.lower_blue = lower_blue
         self.upper_blue = upper_blue
-        
-    def preprocess(self, image_color, image_grayscale):
+
+    def preprocess(self, image_grayscale=None, image_color=None):
+        if image_grayscale is None and image_color is None:
+            raise Exception("No input grayscale and color images!")
         hsv_image = cv2.cvtColor(image_color, cv2.COLOR_BGR2HSV)
         sea_mask = cv2.inRange(hsv_image, self.lower_blue, self.upper_blue)
-        
+
         removed_sea = image_grayscale.copy()
         removed_sea[sea_mask > 0] = [0]
-        
-        return removed_sea
-    
+
+        return sea_mask
+
 
 class SandThresholdingPreprocessor(ImagePreprocessor):
     def __init__(self, threshold) -> None:
         self.threshold = threshold
-        
-    def preprocess(self, image_grayscale):
+
+    def preprocess(self, image_grayscale=None, image_color=None):
+        if image_grayscale is None:
+            raise Exception("No input grayscale image!")
         _, sand_mask = cv2.threshold(image_grayscale, self.threshold, 255, cv2.THRESH_BINARY_INV)
 
         removed_sand = image_grayscale.copy()
@@ -84,14 +97,17 @@ class SandThresholdingPreprocessor(ImagePreprocessor):
 
 
 class SobelPreprocessor(ImagePreprocessor):
-    def __init__(self, gradient_threshold = 200) -> None:
+    def __init__(self, gradient_threshold=200) -> None:
         self.gradient_threshold = gradient_threshold
-        
-    def preprocess(self, image_grayscale):
+
+    def preprocess(self, image_grayscale=None, image_color=None):
+
+        if image_grayscale is None:
+            raise Exception("No input grayscale image!")
         sobel_x = cv2.Sobel(image_grayscale, cv2.CV_64F, 1, 0, ksize=3)
         sobel_y = cv2.Sobel(image_grayscale, cv2.CV_64F, 0, 1, ksize=3)
 
-        gradient_magnitude = np.sqrt(sobel_x**2 + sobel_y**2)
+        gradient_magnitude = np.sqrt(sobel_x ** 2 + sobel_y ** 2)
 
         _, binary_gradient = cv2.threshold(gradient_magnitude, self.gradient_threshold, 255, cv2.THRESH_BINARY)
         binary_gradient = 255 - binary_gradient
@@ -104,8 +120,10 @@ class SobelPreprocessor(ImagePreprocessor):
 class OtsuPreprocessor(ImagePreprocessor):
     def __init__(self) -> None:
         pass
-    
-    def preprocess(self, image_grayscale):
+
+    def preprocess(self, image_grayscale=None, image_color=None):
+        if image_grayscale is None:
+            raise Exception("No input grayscale image!")
         _, binary_image = cv2.threshold(image_grayscale, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
         return binary_image
 
@@ -114,20 +132,23 @@ class PreprocessingPipeline(ImagePreprocessor):
     def __init__(self, processors):
         self.processors = processors
 
-    def preprocess(self, image):
+    def preprocess(self, image_grayscale=None, image_color=None):
+        image = image_grayscale.copy()
         for processor in self.processors:
-            image = processor.preprocess(image)
+            image = processor.preprocess(image_grayscale, image_color)
         return image
-    
 
-class AndBitPreprocessingPipeline(ImagePreprocessor):
-    def __init__(self, pipeline1, pipeline2):
-        self.pipeline1 = pipeline1
-        self.pipeline2 = pipeline2
 
-    def preprocess(self, image):
-        i1, i2 = image.copy(), image.copy()
-        i1 = self.pipeline1.preprocess(i1)
-        i2 = self.pipeline2.preprocess(i2)
-        img = cv2.bitwise_and(i1, i2)
-        return img
+class MultipleANDBitProcessingPipeline(ImagePreprocessor):
+    def __int__(self, pipelines: List[PreprocessingPipeline]):
+        self.pipelines = pipelines
+
+    def preprocess(self, image_grayscale=None, image_color=None):
+        images = []
+        for p in self.pipelines:
+            images.append(p.preprocess(image_grayscale.copy(), image_color.copy()))
+
+        base_img = images[0]
+        for i in range(1, len(images)):
+            base_img = cv2.bitwise_and(base_img, images[i])
+        return base_img
